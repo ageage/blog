2faf71fd5b2211efa72f3d89541125352ca425b0
ageage <ageage0830@gmail.com>
2017-08-08 03:30:22 +0900

    デザインの大枠が完成

---
+++ config/initializers/generators.rb
@@ -0,0 +1,5 @@
+Rails.application.config.generators do |g|
+  g.helper false         # ヘルパーを生成しない
+  g.assets false         # CSS, JavaScript ファイルを生成しない
+  g.skip_routes true     # config/routes.rb を変更しない
+end
