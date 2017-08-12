a70f88677831579a5a3693d572549796c8c8905e
ageage <ageage0830@gmail.com>
2017-08-12 18:59:28 +0900

    管理画面では管理画面用のテンプレートを読み込むように変更。

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,4 +1,4 @@
-class Admin::TopController < ApplicationController
+class Admin::TopController < Admin::ApplicationController
   before_action :authenticate_admin_admin_user!, only: :index
 
   def index
